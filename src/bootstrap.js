936c0ca69c223df198df4db8ac51406faead542d
danielazeveedo2k12 <danielazeveedo2k12@gmail.com>
2020-11-08 00:28:19 -0300

    install underscore lib

--- src/bootstrap.js
+++ src/bootstrap.js
@@ -6,6 +6,7 @@ import TimeStorage from 'time-storage'
 window.myStorage = TimeStorage( PREFX_STORAGE, 6, 'hour')
 
 window.axios = require('axios')
+window._ = require('underscore')
 
 window.axios.defaults.headers.common['X-Requested-With'] = 'XMLHttpRequest'
 window.axios.defaults.headers.common['x-backend-token']  = API_TOKEN
