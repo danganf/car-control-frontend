2b20ce7005aaf3911dca6d4c57e6160008d4e96f
danielazeveedo2k12 <danielazeveedo2k12@gmail.com>
2020-11-08 00:26:03 -0300

    card vehicles home

--- src/pages/Home.vue
+++ src/pages/Home.vue
@@ -9,8 +9,34 @@
             <div class="container">
                 <div class="row">
                     <div class="col-12 n-p-l">
-                        <h1 class="home__title">Meus <b>veículos</b> <i v-if="preloader" class="fas fa-spinner fa-pulse fa-1x"></i></h1>
+                        <h1 class="home__title">
+                            Meus <b>veículos</b>
+                            <i v-if="preloader" class="fas fa-spinner fa-pulse fa-1x"></i>
+                            <button class="filter__btn new-vehicle" type="button">Criar</button>
+                        </h1>
                     </div>
+                </div>
+
+                <div class="row">
+                    <div class="col-12">&nbsp;</div>
+                </div>
+                <div class="row">
+                    <div v-if="!vehicles.length" class="col-12">
+                        <h2 class="feature__title">
+                            Nenhum veículo cadastrado...
+                        </h2>
+                    </div>
+                    <div class="col-12 col-md-4 col-lg-3" v-for="(vehicle, idx) in vehicles" :key="`c-vehicle-card${idx}`">
+                        <div class="price">
+                            <div class="price__item price__item--first"><span>Basic</span> <span>Edit</span></div>
+                            <div class="price__item"><span>7 days</span></div>
+                            <div class="price__item"><span>720p Resolution</span></div>
+                            <div class="price__item"><span>Limited Availability</span></div>
+                            <div class="price__item"><span>Desktop Only</span></div>
+                            <div class="price__item"><span>Limited Support</span></div>
+                            <a href="#" class="price__btn">Selecionar</a>
+                        </div>
+				    </div>
                 </div>
             </div>
         </section>
@@ -28,23 +54,11 @@ export default {
 
   data: () => {
     return {
-        products: [],
+        vehicles: [1,2,3],
         preloader: false
     }
   },
   methods: {
-    async getMyVehicles () {
-        // this.preloader = true
-        // await window.axios.get(process.env.URL_API_BACKEND + 'vehicles')
-        // .then((result) => {
-        //   this.preloader = false
-        //   result.data.splice(0, 1)
-        //   this.products = result.data
-        // }).catch(error => {
-        //   console.log(error)
-        //   this.preloader = false
-        // })
-    }
 
   },
 
@@ -59,6 +73,7 @@ export default {
 </script>
 
 <style scoped>
+    .new-vehicle{display: inline;position: absolute;right: 0;}
     .carrousel-container {margin-left: auto;margin-right: auto;position: relative;list-style: none;padding: 0;overflow: hidden;}
     .swiper-wrapper{display: inline-flex;}
     .home__cover{height: 100%;}
